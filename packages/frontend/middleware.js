6da40f7f79fedf34120d901546f9293300a4f632
dicky.js <dickyke217@gmail.com>
2025-06-29 19:05:36 +0700

    chore: remove unused middleware file

--- packages/frontend/middleware.js
+++
@@ -1,6 +0,0 @@
-// packages/frontend/middleware.js (Setelah Diperbaiki)
-
-// Kosong untuk saat ini, bisa ditambahkan logic middleware kustom nanti.
-// export function middleware(request) {
-//   return NextResponse.next();
-// }
