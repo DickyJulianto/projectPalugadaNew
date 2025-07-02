fd553cdf25446cfa5bae70aeac3d6019d910b83d
dicky.js <dickyke217@gmail.com>
2025-07-02 23:49:46 +0700

    ...

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -21,7 +21,9 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 
 // Middleware   
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json()); // Body parser
 app.use(passport.initialize()); // Inisialisasi Passport
 
